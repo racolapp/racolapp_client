0add87d0d80531507c2aa6d2c6e2e4f0ebb71113
BLECUONA <benjaminlecuona@gmail.com>
2019-05-01 17:56:59 +0200

    feat(Android Geoloc Map): isolated in component

--- App.js
+++ App.js
@@ -1,106 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
-import {PermissionsAndroid} from 'react-native';
-// import GoogleMaps from './src/components/GoogleMaps'
+import GoogleMaps from './src/components/GoogleMaps'
 
-// ANDROID PERMISSION FOR API >= 23
-async function requestLocationPermission() {
-  try {
-    const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
-    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      console.log('You can use the location');
-    } else {
-      console.log('Location permission denied');
-    }
-  } catch (err) {
-    console.warn(err);
-  }
-}
+// // (UTILE ?????) ANDROID PERMISSIONS FOR API >= 23
+// import {requestLocationPermission} from './src/utils/PermissionsAndroid';
 
 export default class App extends Component {
 
-  constructor(props) {
-    super(props);
+  // async componentWillMount(){
+  //   await requestLocationPermission();
+  // }
 
-    this.state = {
-      latitude: null,
-      longitude: null,
-      error: null,
-    };
-  }
-
-  async componentDidMount() {
-    console.log("ComponentDidMount");
-    await requestLocationPermission()
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log("latitude: " + position.coords.latitude);
-        console.log("longitude: " + position.coords.longitude);
-        this.setState({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          error: null
-        });
-      },
-      (error) => {
-        console.log("ERROR");
-        this.setState({error: error.message})
-      },
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
-    );
-    console.log("EndComponentDidMount");
-  }
-
-  render() {
+  render(){
     return (
-      this.state.latitude !== null && <MapView 
-        style={styles.container} 
-        provider={PROVIDER_GOOGLE}
-        initialRegion={{
-          latitude: this.state.latitude,
-          longitude: this.state.longitude,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01
-        }}
-        showsUserLocation={true}
-      />
-    );
+      <>
+        {/* ATTENTION: le component GoogleMaps n'affichera rien si wrappé dans autre chose que des chevrons vides */}
+        <GoogleMaps/>
+      </>
+    )
   }
 }
 
-//   render(){
-//     return (
-//       <View style={styles.container}>
-//         <GoogleMaps/>
-//       </View>
-//     )
-//   }
-// }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -1,21 +1,6 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
-import {PermissionsAndroid} from 'react-native';
-
-// ANDROID PERMISSION FOR API >= 23
-async function requestLocationPermission() {
-    try {
-      const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log('You can use the location');
-      } else {
-        console.log('Location permission denied');
-      }
-    } catch (err) {
-      console.warn(err);
-    }
-  }
 
 export default class GoogleMaps extends Component {
 
@@ -29,45 +14,35 @@ export default class GoogleMaps extends Component {
       };
     }
   
-    async componentWillMount(){
-      await requestLocationPermission()
-    }
-  
-    componentDidMount() {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          console.log("latitude: " + position.coords.latitude);
-          console.log("longitude: " + position.coords.longitude);
-          this.setState({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-            error: null
-          })
-        },
-        (error) => {
-            console.log("ERROR");
-            this.setState({error: error.message});
-            
-        },
-        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
-      );
-    }
-  
+    async componentWillMount() {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            this.setState({
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+              error: null
+            });
+          },
+          (error) => {
+            this.setState({error: error.message})
+          },
+          { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+        );
+      }
+      
     render() {
       return (
-        <View style={styles.container}>
-        {this.state.latitude !== null && <MapView 
-          style={styles.container} 
-          provider={PROVIDER_GOOGLE}
-          initialRegion={{
-              latitude: this.state.latitude,
-              longitude: this.state.longitude,
-              latitudeDelta: 0.01,
-              longitudeDelta: 0.01
+        this.state.latitude !== null && <MapView 
+            style={styles.container} 
+            provider={PROVIDER_GOOGLE}
+            initialRegion={{
+                latitude: this.state.latitude,
+                longitude: this.state.longitude,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01
             }}
             showsUserLocation={true}
-            />}
-            </View>
+        />
       );
     }
   }

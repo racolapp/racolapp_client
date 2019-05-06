fbb84a55f69a0228098ec010e0d68f450c755ea9
BLECUONA <benjaminlecuona@gmail.com>
2019-05-06 16:03:27 +0200

    unusable -- configuring stack and tab navigation

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import HomeScreen from './src/screens/Home';
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import { Text, View } from 'react-native';
+import { createBottomTabNavigator, createStackNavigator, createAppContainer } from "react-navigation";
 
 
 const AppNavigator = createStackNavigator({
@@ -9,4 +10,28 @@ const AppNavigator = createStackNavigator({
   }
  });
 
- export default createAppContainer(AppNavigator);
+//  export default createAppContainer(AppNavigator);
+
+
+
+// class HomeScreen extends React.Component {
+//   render() {
+//     return (
+//       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//         <Text>Home!</Text>
+//       </View>
+//     );
+//   }
+// }
+
+
+
+const TabNavigator = createBottomTabNavigator({
+  Home: HomeScreen,
+  EventsPosted: EventsPostedScreen,
+  Add: AddScreen,
+  EventsSubscripted: EventsSubscriptedScreen,
+  Profile: ProfileScreen
+});
+
+export default createAppContainer(TabNavigator);
---
+++ src/screens/Add/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+export default class SettingsScreen extends React.Component {
+    render() {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Add an event!</Text>
+        </View>
+      );
+    }
+  }

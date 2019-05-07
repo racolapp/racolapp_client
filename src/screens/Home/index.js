7dda69010c30203b9e06aebd4de7e91e07aca1db
Marc DIDIER <20180221@B15-08.local>
2019-05-07 09:14:38 +0200

    move the google autocomplete into a component

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
 import GoogleMaps from '../../components/GoogleMaps';
-import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
-import Config from 'react-native-config'
+import GoogleAutocomplete from '../../components/GoogleAutocomplete'
+
 
 export default class HomeScreen extends Component {
   constructor(props) {
@@ -62,100 +62,11 @@ export default class HomeScreen extends Component {
     return { latitude, longitude, latitudeDelta, longitudeDelta };
   };
 
-  _renderGooglePlacesAutocomplete = () => {
-    const { displayResultGooglePlacesSearch } = this.state.googlePlaces;
-    const { latitudeDelta, longitudeDelta } = this.state.location;
-    return (
-      <GooglePlacesAutocomplete
-        placeholder="Search"
-        minLength={2} // minimum length of text to search
-        autoFocus={false}
-        returnKeyType={"search"} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
-        keyboardAppearance={"light"} // Can be left out for default keyboardAppearance https://facebook.github.io/react-native/docs/textinput.html#keyboardappearance
-        listViewDisplayed={displayResultGooglePlacesSearch} // true/false/undefined
-        fetchDetails={true}
-        enablePoweredByContainer={false}
-        renderDescription={row =>
-          row.description || row.formatted_address || row.name
-        } // (TODO: vérifier) custom description render
-        onPress={(data, details = null) => {
-          this.setState({
-            location:{
-              latitude: details.geometry.location.lat,
-              longitude: details.geometry.location.lng,
-              latitudeDelta,
-              longitudeDelta
-            }
-          });
-        }}
-        getDefaultValue={() => ""}
-        // getDefaultValue={() => this.state.textInput}
-        query={{
-          // available options: https://developers.google.com/places/web-service/autocomplete
-          key: Config.GOOGLE_MAPS_API_KEY,
-          language: "fr", // (TODO: vérifier) language of the results
-          types: "(cities)" // default: 'geocode'
-        }}
-        styles={{
-          textInputContainer: {
-            width: "100%",
-            backgroundColor: "rgba(255, 255, 255, 0)",
-            borderBottomWidth: 0
-          },
-          description: {
-            fontWeight: "bold"
-          },
-          listView: {
-            backgroundColor: "white",
-            borderRadius: 10
-          }
-        }}
-        currentLocation={false} // Will add a 'Current location' button at the top of the predefined places list
-        // // currentLocationLabel="Current location"
-        // nearbyPlacesAPI="GooglePlacesSearch" // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
-        // GoogleReverseGeocodingQuery={
-        //   {
-        //     // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
-        //   }
-        // }
-        // GooglePlacesSearchQuery={{
-        //   // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-        //   rankby: "distance",
-        //   type: "cafe"
-        // }}
-        // GooglePlacesDetailsQuery={{
-        //   // available options for GooglePlacesDetails API : https://developers.google.com/places/web-service/details
-        //   fields: "formatted_address"
-        // }}
-        // filterReverseGeocodingByTypes={[
-        //   "locality",
-        //   "administrative_area_level_3"
-        // ]} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-        // // predefinedPlaces={[homePlace, workPlace]}
-
-        debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
-        renderLeftButton={() => (
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              Keyboard.dismiss();
-              this.setState({
-                googlePlaces: {
-                  displayResultGooglePlacesSearch: "false"
-                }
-              });
-            }}
-          >
-            <Text> Left Button or Left Icon </Text>
-          </TouchableOpacity>
-        )}
-      />
-    );
-  };
+  
   render(){
     return (
       <>
-        {this._renderGooglePlacesAutocomplete()}
+        <GoogleAutocomplete googlePlaces={this.state.googlePlaces} location={this.state.location} />
         {/* ATTENTION: le component GoogleMaps n'affichera rien si wrappé dans autre chose que des chevrons vides */}
         <GoogleMaps region={this._setRegion()}/>
         <View style={styles.bottomContainer}>
